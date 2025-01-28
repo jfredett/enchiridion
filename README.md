c0dd7ef87b00972770accac5c0dceedfa2e388ca
Joe Fredette <jfredett@gmail.com>
2025-01-27 20:56:20 -0500

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# `enchiridion`
+
+My handy k8s operators. Mostly an excuse to learn Go.
+
+## Operators
+
+- stockfish operator: create a 'ChessAnalysis' resource which uses stockfish to analyze a chess game (contained in the
+resource) and create a markdown report of it's analysis saved to a configmap.
+
---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Keep it handy";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = { self, nixpkgs, devenv, ... } @ inputs:
+    let
+      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = f: builtins.listToAttrs (map (name: { inherit name; value = f name; }) systems);
+    in
+      {
+      packages.x86_64-linux.devenv-up = self.devShells.x86_64-linux.default.config.procfileScript;
+      devShells = forAllSystems
+        (system: let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+
+            modules = [{
+              languages.go.enable = true;
+
+              packages = with pkgs; [
+                bacon
+                operator-sdk
+                cloc
+                just
+                linuxKernel.packages.linux_6_6.perf
+                perf-tools
+              ];
+            }];
+          };
+        });
+    };
+}
